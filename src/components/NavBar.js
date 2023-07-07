6fd823f1d24e58a31147eac14051d9a70a219534
Zhongming <atenliao@gmail.com>
2023-07-06 20:40:29 -0700

    update navbar color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,12 @@ import {NavLink} from "react-router-dom";
 
 function NavBar(){
     return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggle-icon"></span>
                 </button>
-                <NavLink className="navbar-brand fs-3" to="/" exact>Supper Pizza</NavLink>
-                <div className="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+                <NavLink className="navbar-brand fs-2" to="/" exact>Supper Pizza</NavLink>
+                <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                 <NavLink className = "nav-item nav-link fs-4" to='/' exact>
                     Home
